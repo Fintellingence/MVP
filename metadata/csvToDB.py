c7352f415386fa4b34d8284c6f7b6c68827bce5a
D7-Brane <g.n.nagaoka@gmail.com>
2021-01-13 19:30:50 -0300

    News database generated

--- metadata/csvToDB.py
+++ metadata/csvToDB.py
@@ -11,14 +11,33 @@ def get_tickers(file):
     stripper = lambda x: (x.strip())
     return list(map(stripper, tickers))
 
+def recomendations_to_database():
+    conn = sqlite3.connect('Recommendations.db')
+    for ticker in tickers:
+        tickerDir = CWD+'/'+ticker
+        os.chdir(tickerDir)
+        ticker_df = pd.read_csv(ticker+'Recommendations.csv').set_index('DateTime')
+        ticker_df.to_sql(ticker, con =conn, if_exists='replace')
+        print('Recommendation history for '+ticker+" successfully saved in Reccomendations.db")
+    conn.close()
+    return 0
+
+def news_to_database():
+    conn = sqlite3.connect('News.db')
+    for ticker in tickers:
+        tickerDir = CWD+'/'+ticker
+        os.chdir(tickerDir)
+        ticker_df = pd.read_csv(ticker+'News.csv').set_index('DateTime')
+        ticker_df.to_sql(ticker, con =conn, if_exists='replace')
+        print('Recommendation history for '+ticker+" successfully saved in Reccomendations.db")
+    conn.close()
+    return 0
+
+
+
 tickers = get_tickers('topBRinNYSE.txt')
 CWD = os.getcwd()
-conn = sqlite3.connect('Recommendations.db')
+news_to_database()
 
-for ticker in tickers:
-    tickerDir = CWD+'/'+ticker
-    os.chdir(tickerDir)
-    ticker_df = pd.read_csv(ticker+'Recommendations.csv').set_index('DateTime')
-    ticker_df.to_sql(ticker, con =conn, if_exists='replace')
-    print('Recommendation histpry for '+ticker+" successfully saved in Reccomendations.db")
-conn.close()
+
+
