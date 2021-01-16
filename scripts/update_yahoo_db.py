24babcd5f1518d54f8944cf3efe93b22f6c7ffa4
andriati-alex <andriati.alex@gmail.com>
2021-01-16 12:10:03 -0300

    script update_yahoo_db.py improved with default values for the command line arguments given by builder global variables. Additional help about the variables provided

--- scripts/update_yahoo_db.py
+++ scripts/update_yahoo_db.py
@@ -3,14 +3,16 @@ import argparse
 from mvp import builder
 
 
-def run(db_path, company_path):
+def run(db_path, sym_path):
     yahoo = builder.Yahoo(db_path)
-    yahoo.update(company_path)
+    yahoo.update(sym_path)
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("db_path", type=str)
-    p.add_argument("company_path", type=str)
+    p.add_argument("-db_path", dest="db_path", type=str,
+            default=builder.DEFT_D1_DB_PATH, help="path to database file")
+    p.add_argument("-sym_path", dest="sym_path", type=str,
+            default=builder.DEFT_SYMBOLS_PATH, help="path to symbols txt file")
     args = p.parse_args()
     run(**vars(args))
