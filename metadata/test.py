720a20ed2f176123c3258f9d82234db529ffcfa9
D7-Brane <g.n.nagaoka@gmail.com>
2021-01-17 20:45:32 -0300

    removed undesired files

--- metadata/test.py
+++
@@ -1,3 +0,0 @@
-
-for i in zip([1,2],['a','b']):
-    print(i[0])
--- metadata/waybackAuto.sh
+++
@@ -1,4 +0,0 @@
-string="$"
-for url in $(cat finvizURLs.txt); do
-  wayback-machine-scraper -a "\'$url\$\'" -f 20100101 -t 20200101 $url -v &> output.txt
-done
