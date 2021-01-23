eb05fcee1904d9b8589ea95bd304eaa653c4c322
andriati-alex <andriati.alex@gmail.com>
2021-01-23 19:28:56 -0300

    Introduced function to read data from yahoo database

--- mvp/rawdata.py
+++ mvp/rawdata.py
@@ -1,13 +1,15 @@
 import os
 import pandas as pd
+import datetime as dt
 import sqlite3 as sql3
+import pandas_datareader as pdr
 
 
 class RawData:
     """
-    Class to read symbol from database and set as data-frame. Provide methods
-    to refactor the data-frame in other formats than linearly time spaced bar
-    which may exhibit better statistical properties.
+    Class to read symbol from minute-1 database and set as data-frame. Provide
+    methods to refactor the data-frame in other formats than linearly time
+    spaced bar which may exhibit better statistical properties.
     """
 
     def __init__(self, symbol, db_path):
@@ -33,6 +35,7 @@ class RawData:
                 "symbol {} not found in database {}".format(symbol, db_path)
             )
 
+
     def __get_data_from_db(self):
         conn = sql3.connect(self.db_path)
         df = pd.read_sql("SELECT * FROM {}".format(self.symbol), conn)
@@ -41,7 +44,8 @@ class RawData:
         df.drop(["DateTime"], axis=1, inplace=True)
         return df
 
-    def tick_bars_df(self, bar_size_th=1000):
+
+    def tick_bars(self, bar_size_th=1000):
         """
         Convert 1-minute time spaced data-frame to
         (approximately) `bar_size_th` ticks spaced
@@ -50,6 +54,9 @@ class RawData:
         ---------
         `bar_size_th` : ``int``
             number of ticks/deals to form a new candle-stick (data-frame row)
+
+        Return
+        `new_df` : ``pandas.DataFrame``
 
         """
         accum_ticks = 0
@@ -99,7 +106,7 @@ class RawData:
         newDF.index.name = "BarOpenTime"
         return newDF
 
-    def volume_bars_df(self, bar_size_th=1e6):
+    def volume_bars(self, bar_size_th=1e6):
         """
         Convert 1-minute time spaced data-frame to
         (approximately) `bar_size_th` volume spaced
@@ -108,6 +115,9 @@ class RawData:
         ---------
         `bar_size_th` : ``int``
             volume required to form a new candle-stick (data-frame row)
+
+        Return
+        `new_df` : ``pandas.DataFrame``
 
         """
         accum_vol = 0
@@ -158,7 +168,7 @@ class RawData:
         return new_df
 
 
-    def money_bars_df(self, bar_size_th=1e6):
+    def money_bars(self, bar_size_th=1e6):
         """
         Convert 1-minute time spaced data-frame to
         (approximately) `bar_size_th` money spaced
@@ -167,6 +177,9 @@ class RawData:
         ---------
         `bar_size_th` : ``int``
             money volume required to form a new candle-stick (data-frame row)
+
+        Return
+        `new_df` : ``pandas.DataFrame``
 
         """
         money = 0
@@ -219,3 +232,75 @@ class RawData:
         )
         new_df.index.name = "BarOpenTime"
         return new_df
+
+
+    def daily_bars(self):
+        """
+        Convert 1-minute time spaced data-frame to daily spaced
+
+        Return
+        `new_df` : ``pandas.DataFrame``
+
+        """
+        last_index = 0
+        n_bars = 0
+        bar_list = []
+        date_index = []
+        current_day = self.df.index[0].day
+        for idx in range(self.df.index.size):
+            if self.df.index[idx].day != current_day:
+                bar_max = self.df.iloc[last_index:idx]["High"].max()
+                bar_min = self.df.iloc[last_index:idx]["Low"].min()
+                bar_vol = self.df.iloc[last_index:idx]["Volume"].sum()
+                bar_tck = self.df.iloc[last_index:idx]["TickVol"].sum()
+                bar_opn = self.df.iloc[last_index]["Open"]
+                bar_cls = self.df.iloc[idx - 1]["Close"]
+                bar_date = self.df.index[idx - 1].date()
+                bar_list.append(
+                    [bar_opn, bar_max, bar_min, bar_cls, bar_tck, bar_vol]
+                )
+                date_index.append(bar_date)
+                current_day = self.df.index[idx].day
+                last_index = idx
+                n_bars = n_bars + 1
+        date_index = pd.to_datetime(date_index)
+        new_df = pd.DataFrame(
+            bar_list,
+            columns=["Open", "High", "Low", "Close", "TickVol", "Volume"],
+            index=date_index,
+        )
+        new_df.index.name = "Date"
+        return new_df
+
+
+class DailyDataYahoo():
+    """
+    Class to read symbol from day-1 Yahoo database and set as data-frame
+    """
+
+    def __init__(self, symbol, db_path):
+        self.symbol = symbol
+        self.db_path = db_path
+        self.df = self.__get_data_from_db()
+
+    def __get_data_from_db(self):
+        conn = sql3.connect(self.db_path)
+        try:
+            df = pd.read_sql("SELECT * FROM {}".format(self.symbol), conn)
+            df.index = pd.to_datetime(df["Date"])
+            df.drop(["Date"], axis=1, inplace=True)
+        except Exception as e:
+            init_day = dt.date(2010, 1, 2)
+            final_day = dt.date.today()
+            print(e, "Trying to download with pandas-datareader"
+                    " from {} to {}".format(init_day, final_day)
+            )
+            df = pdr.DataReader(
+                    self.symbol + ".SA",
+                    "yahoo",
+                    init_day,
+                    final_day
+            )
+            df.rename(columns={"Adj Close": "AdjClose"}, inplace=True)
+        conn.close()
+        return df
