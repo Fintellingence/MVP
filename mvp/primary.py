00b77125f647011ae358c6a83161baf48e940e1b
D7-Brane <g.n.nagaoka@gmail.com>
2021-02-01 17:35:34 -0300

    WIP get_labels_standard

--- mvp/primary.py
+++ mvp/primary.py
@@ -5,6 +5,7 @@ import numpy as np
 
 class PrimaryModel:
     def __init__(self, raw_data, model_type, parameters):
+
         self.model_type = model_type
         self.model_parameters = parameters["ModelParameters"]
         self.operation_parameters = parameters["OperationParameters"]
@@ -44,8 +45,42 @@ class PrimaryModel:
         )
         return events_MA
 
-    def classical_filter(self):
+    def events_classical_filter(self):
         pass
 
-    def bollinger(self):
+    def events_bollinger(self):
+        pass
+
+    def get_labels_standard(self, events):
+        stop_loss = self.operation_parameters["SL"]
+        take_profit = self.operation_parameters["TP"]
+        horizon = self.operation_parameters["IH"]
+
+        event_date_time = events.keys()
+        close_data = self.feature_data.df_curated["Close"].reset_index()
+        for event in event_date_time[:2]:
+            signal_position = close_data.index[
+                close_data["DateTime"] == event
+            ][0]
+            horizon_data = (
+                close_data.iloc[signal_position : signal_position + horizon]
+                .reset_index()
+                .drop(columns="index")
+            )
+            profit_touch = pd.DataFrame()
+            loss_touch = pd.DataFrame()
+            if event == 1:
+                profit_touch = horizon_data[
+                    horizon_data["Close"].gt(
+                        horizon_data["Close"][0] + take_profit
+                    )
+                ]
+                print(horizon_data["Close"][0])
+                loss_touch = horizon_data[
+                    horizon_data["Close"].lt(
+                        horizon_data["Close"][0] + stop_loss
+                    )
+                ]
+            print(profit_touch)
+            print(loss_touch)
         pass
