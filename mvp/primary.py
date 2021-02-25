7b48c6e00b5a8478b1027e75d1d281e47903aff3
D7-Brane <g.n.nagaoka@gmail.com>
2021-02-25 18:42:23 -0300

    Cleaning Primary
    New attribute events_df, it is the input of Labels.labels

--- mvp/primary.py
+++ mvp/primary.py
@@ -58,6 +58,7 @@ class PrimaryModel:
         self.feature_data = mvp.curated.CuratedData(
             raw_data, parameters["ModelParameters"]
         )
+        self.events_df = self.get_events_dataframe()
 
     def sign(self, x):
         if x >= 0:
@@ -79,50 +80,6 @@ class PrimaryModel:
             return -1
 
         return np.nan
-
-    def horizon_data(self, event_datetime):
-        horizon = self.operation_parameters["IH"]
-        close_data = self.feature_data.df_curated["Close"].reset_index()
-        signal_position = close_data[
-            close_data["DateTime"] == event_datetime
-        ].index[0]
-        horizon_data = (
-            close_data.iloc[signal_position : signal_position + horizon]
-            .reset_index()
-            .drop(columns="index")
-        )
-        return horizon_data
-
-    def touches(self, horizon_df):
-        stop_loss = self.operation_parameters["SL"]
-        take_profit = self.operation_parameters["TP"]
-        profit_touch = None
-        loss_touch = None
-        entry_position = horizon_df.iloc[0]["Close"]
-        profits = horizon_df[
-            horizon_df["Close"].gt(entry_position + take_profit)
-        ]
-        losses = horizon_df[horizon_df["Close"].lt(entry_position - stop_loss)]
-        if not profits.empty:
-            profit_touch = profits.iloc[0]["DateTime"]
-        if not losses.empty:
-            loss_touch = losses.iloc[0]["DateTime"]
-        touches = list(
-            map(
-                lambda x: dt.datetime(3000, 1, 1, 0, 0, 0) if x == None else x,
-                [profit_touch, loss_touch],
-            ),
-        )
-        return touches
-
-    def labels(self, touches, event_trigger):
-        if touches[0] == touches[1]:
-            return 0
-        else:
-            if touches[0] < touches[1]:
-                return event_trigger
-            else:
-                return -event_trigger
 
     def events_crossing_MA(self):
         """
@@ -330,40 +287,6 @@ class PrimaryModel:
 
         return pd.DataFrame(events_bol).reset_index()
 
-    def event_labels(self, events_df):
-        """
-        Given a dataframe events_df, containing an index column 'DateTime' and a 'Trigger' column
-        containing the event side (Buy = 1 or Sell = -1) this method returns a list of the triple label:
-            1 if take profit was achieved
-            0 if investment horizon was achieved
-           -1 if stop loss was achieved
-        for each event in events_df.
-
-        Parameters
-        ----------
-        `events_df`: ``pd.DataFrame()``
-            columns = ['DateTime','Trigger']
-            'DateTime' is the pd.index column, containing pd.Timestamp() values for the calculated events.
-            'Trigger' is a coulmn containing the side of the stratgey: Buy = 1, Sell = -1.
-
-        Modified
-        ----------
-        None
-
-        Return
-        ----------
-
-        ``list``
-            A list containing a label (1, 0, or -1) for each of the triggers in events_df.
-        """
-        labels = []
-        for event in events_df.values:
-            event_datetime = event[0]
-            event_trigger = event[1]
-            horizon_df = self.horizon_data(event_datetime)
-            labels.append(self.labels(self.touches(horizon_df), event_trigger))
-        return labels
-
     def events(self):
         if self.model_type == "crossing-MA":
             return self.events_crossing_MA()
@@ -373,3 +296,12 @@ class PrimaryModel:
             return self.events_classical_filter(
                 self.model_parameters["threshold"]
             )
+
+    def get_events_dataframe(self):
+        return pd.concat(
+            [
+                self.feature_data.df_curated[["Close"]],
+                self.events().set_index("DateTime"),
+            ],
+            axis=1,
+        )
