2b9ec78a2a0069bccd75edc5881752b2cb2f4ed3
andriati-alex <andriati.alex@gmail.com>
2021-03-11 19:28:04 -0300

    Name of curated.py module changed to refined_data.py
    Just for a better syntax to be consistent with the class RefinedData

--- mvp/__init__.py
+++ mvp/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "0.1.0"
 
 from mvp import curated_builder
-from mvp import curated
 from mvp import rawdata
+from mvp import refined_data
 from mvp import data_builder
 from mvp import draw
 from mvp import primary
