990e8fdf5fc2d3f9d68b9d606349578fe9847390
andriati-alex <andriati.alex@gmail.com>
2021-03-24 21:21:50 -0300

    Error handling when one tries to repeat features

--- mvp/portfolio_manager.py
+++ mvp/portfolio_manager.py
@@ -9,14 +9,15 @@ class RefinedSet:
     """
     Class to collect a set of stock shares and obtain common features
     For more info about the features see `RefinedData` class. Note
-    that despite there are advisable to restricted the analysis to the
-    common features requested in the constructor, the refined objects
-    can still be acessed through the attributes below
+    that despite the possibility to compute new features acessing the
+    refined objects by the `refined_obj` attribute, it is advisable to
+    restricted the analysis to the common features requested in the
+    constructor.
 
     Main attributes
     ---------------
     `refined_obj` : ``dict {str : RefinedData}``
-        set of refined data objects acessed by the symbol as key
+        set of refined data objects acessed using the symbol as key
     `symbol_period` = ``dict {str : (pandas.Timestamp, pandas.Timestamp)}``
         (start, stop) period the refined object was analyzed
 
@@ -63,7 +64,6 @@ class RefinedSet:
             returns) and 0.5.
 
         """
-        dictionary of CuratedData objects with SYMBOL as key values.
         if not os.path.isfile(db_path):
             raise IOError("Database file {} not found".format(db_path))
         self.db_path = db_path
@@ -71,26 +71,25 @@ class RefinedSet:
             "MA": "get_simple_MA",
             "DEV": "get_deviation",
             "RSI": "get_RSI",
-            "FRAC_DIFF": "frac_diff",
+            "FRACDIFF": "frac_diff",
             "AUTOCORRELATION": "moving_autocorr",
         }
-        self.raw_input_string = common_features
-        self.time_intervals = self.__extract_intervals()
+        self.available_time_intervals = [1, 5, 10, 15, 30, 60, "day"]
+        self.raw_input_string = self.__assert_input_string(common_features)
+        self.time_intervals = self.__extract_intervals(self.raw_input_string)
         self.input_dict = self.__convert_input_dict()
         self.refined_obj = {}
         self.symbol_period = {}
 
-    def __assert_input_string(self):
+    def __assert_input_string(self, input_string):
         """
         validade the input string with common features. If it is not
         in agreement with the standards raise ValueError
         """
-        key_value_list_split = self.raw_input_string.split(":")
+        key_value_list_split = input_string.split(":")
         if len(key_value_list_split) % 2 != 0:
             raise ValueError(
-                "Wrong pairings divided by colons in {}".format(
-                    self.raw_input_string
-                )
+                "Wrong pairings divided by colons in {}".format(input_string)
             )
         keys = key_value_list_split[::2]
         for key in keys:
@@ -98,29 +97,30 @@ class RefinedSet:
                 raise ValueError(
                     "Each key-code must have only one '_' separating "
                     "feature abbreviation and interval. Check out {}".format(
-                        self.raw_input_string
+                        input_string
                     )
                 )
             attr_abbr_key = key.split("_")[0]
             if attr_abbr_key not in self.__refined_attr.keys():
                 raise ValueError(
                     "Found invalid abbreviation {} in {}".format(
-                        attr_abbr_key, self.raw_input_string
+                        attr_abbr_key, input_string
                     )
                 )
-        intervals = self.__extract_intervals()
+        intervals = self.__extract_intervals(input_string)
         if not set(intervals).issubset(set(self.available_time_intervals)):
             raise ValueError(
-                "There are not available time intervals in {}".format(
-                    self.raw_input_string
+                "There are invalid available time intervals in {}".format(
+                    input_string
                 )
             )
+        return self.__remove_repetitions(input_string)
 
-    def __extract_intervals(self):
+    def __extract_intervals(self, input_string):
         """
         Return list with appropriate interval datatypes from input string
         """
-        key_interval_codes = self.raw_input_string.split(":")[::2]
+        key_interval_codes = input_string.split(":")[::2]
         string_intervals = [key.split("_")[1] for key in key_interval_codes]
         intervals = []
         for string_interval in string_intervals:
@@ -132,12 +132,42 @@ class RefinedSet:
                 intervals.append(time_interval)
         return intervals
 
+    def __remove_repetitions(self, input_string):
+        """
+        Remove any duplicated feature of raw input string
+        """
+        key_value_list_split = input_string.split(":")
+        keys = key_value_list_split[::2]
+        str_vals = key_value_list_split[1::2]
+        no_rep_map = {}
+        for key, str_val in zip(keys, str_vals):
+            if key in no_rep_map.keys() and str_val:
+                raw_str_val = no_rep_map[key] + "," + str_val
+            else:
+                raw_str_val = str_val
+            try:
+                num_vals = list(map(int, set(raw_str_val.split(","))))
+            except Exception as ex:
+                num_vals = list(map(float, set(raw_str_val.split(","))))
+            num_vals.sort()
+            str_val_unique = ",".join(map(str, num_vals))
+            no_rep_map[key] = str_val_unique
+        no_rep_list = []
+        for no_rep_key, no_rep_val in no_rep_map.items():
+            no_rep_list.append(no_rep_key)
+            no_rep_list.append(no_rep_val)
+        no_rep_input_string = ":".join(no_rep_list)
+        return no_rep_input_string
+
     def __convert_input_dict(self):
         """
-        Process the input string with features to be computed in
-        a dictionary suitable to create `RefinedData` objects,
-        separating by colon the input string and casting to
-        correct data types
+        Process the input (raw)string with features to be computed
+
+        Return
+        ------
+        ``dict`` {"KEY_T" : [ value1, value2, ... , valueN }
+            where "KEY" is a method abbreviation and "T" a period
+
         """
         key_value_list_split = self.raw_input_string.split(":")
         keys = key_value_list_split[::2]
@@ -153,9 +183,24 @@ class RefinedSet:
 
     def is_empty(self):
         """
-        Return `True` if there are any symbols refined in this object
+        Return `True` if there are no symbols refined in this object
         """
         return not self.refined_obj
+
+    def add_common_features(self, new_input_string):
+        try:
+            self.raw_input_string = self.__assert_input_string(
+                self.raw_input_string + ":" + new_input_string
+            )
+        except ValueError as err:
+            print(
+                err,
+                "An error occurred while appending the new string {}".format(
+                    new_input_string
+                ),
+                sep="\n\n",
+            )
+            return
 
     def new_symbol(self, symbol, start=None, stop=None):
         """
