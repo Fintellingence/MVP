ee3215d35357cd1524c51d060776b14ad01d55f5
Caio Dadauto <caiodadauto@gmail.com>
2021-04-17 17:24:33 -0300

    implement the last fitting process based on the best hyper-parameters

--- mvp/metamodel.py
+++ mvp/metamodel.py
@@ -6,6 +6,7 @@ from datetime import datetime as dt
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 import tensorflow.summary as summary
 from tensorboard.plugins.hparams import api as hp
@@ -79,7 +80,6 @@ class BagingModelOptimizer:
         min_time_weight=0.25,
         time_weights_fn=time_weights,
         samples_weights_fn=sample_weights,
-        log_dir="logs",
         metrics={
             "Precision": (precision_score, False),
             "F1": (f1_score, False),
@@ -87,7 +87,6 @@ class BagingModelOptimizer:
             # "AUC": (auc, False),
         },
     ):
-        self._log_dir = log_dir
         self._metrics = metrics
         self._run_dir = run_dir
         self._use_weight = use_weight
@@ -102,21 +101,48 @@ class BagingModelOptimizer:
             os.cpu_count() if num_of_threads is None else num_of_threads
         )
         self._best_kwargs_model = None
-        self._best_kwargs_scaler = None
-        self._file_log = create_log_file("metamodel", log_dir=self._log_dir)
+        self._best_kwargs_scaler = {}
+        self._file_log = create_log_file("metamodel", log_dir=self._run_dir)
+        if self._use_weight:
+            if (self._time_weights_fn is None) and (
+                self._samples_weights_fn is None
+            ):
+                raise ValueError(
+                    "With `use_weight` is True, at least one of the functions,"
+                    " `time_weight_fn` or `sample_weights_fn`, have to be defined"
+                )
 
-    def fit(
+    def __is_better(self, last, best):
+        for l, b in zip(last, best):
+            if l > b:
+                return True
+        return False
+
+    def __set_model(
         self,
-        data,
-        labels,
-        horizon,
-        kwargs_model=None,
-        kwargs_scaler=None,
-        validation=True,
-        return_roc=True,
-        return_partial_labels=True,
+        kwargs_scaler,
+        kwargs_model,
+        closed,
+        train_horizon,
+        test_horizon=None,
     ):
-        pass
+        if kwargs_scaler is None:
+            kwargs_scaler = {}
+        if self._scaler_pipeline is not None:
+            scaler = self._scaler_pipeline.set_params(**kwargs_scaler)
+        else:
+            scaler = None
+        if self._use_weight:
+            train_weight = self.get_weight(closed, train_horizon)
+            if test_horizon is not None:
+                test_weight = self.get_weight(closed, test_horizon)
+            else:
+                test_weight = None
+        else:
+            train_weight = None
+            test_weight = None
+        model = self._model_fn(**kwargs_model)
+        return model, scaler, train_weight, test_weight
 
     def __partial_fit_eval(
         self,
@@ -130,20 +156,14 @@ class BagingModelOptimizer:
         kwargs_model,
         kwargs_scaler=None,
     ):
-        if kwargs_scaler is None:
-            kwargs_scaler = {}
-        if self._scaler_pipeline is not None:
-            scaler = self._scaler_pipeline.set_params(**kwargs_scaler)
-        else:
-            scaler = None
-        if self._use_weight:
-            train_weight = self.get_weight(closed, horizon.iloc[train_idx])
-            test_weight = self.get_weight(closed, horizon.iloc[test_idx])
-        else:
-            train_weight = None
-            test_weight = None
-        model = self._model_fn(**kwargs_model)
-
+        model, scaler, train_weight, test_weight = self.__set_model(
+            kwargs_scaler,
+            kwargs_model,
+            closed,
+            horizon[train_idx],
+            horizon[test_idx],
+        )
+        outcomes = {}
         x_train, x_test = data[train_idx], data[test_idx]
         y_train, y_test = labels[train_idx], labels[test_idx]
         train_horizon = np_horizon[train_idx]
@@ -151,7 +171,6 @@ class BagingModelOptimizer:
             x_train = scaler.fit_transform(x_train)
         if scaler is not None:
             x_test = scaler.transform(x_test)
-
         model.fit(
             x_train,
             y_train,
@@ -159,8 +178,6 @@ class BagingModelOptimizer:
             horizon=train_horizon,
         )
         predicted = model.predict(x_test)
-
-        outcomes = {}
         for name, (metric_fn, use_weight) in self._metrics.items():
             if use_weight:
                 outcomes[name] = metric_fn(
@@ -168,25 +185,121 @@ class BagingModelOptimizer:
                 )
             else:
                 outcomes[name] = metric_fn(y_test, predicted)
-        return outcomes, model, predicted
+        return outcomes, predicted, horizon.index[test_idx]
 
-    def __is_better(self, last, best):
-        for l, b in zip(last, best):
-            if l > b:
-                return True
-        return False
+    def __fit(
+        self,
+        x_train,
+        y_train,
+        horizon,
+        np_horizon,
+        closed,
+        kwargs_model,
+        kwargs_scaler=None,
+    ):
+        model, scaler, train_weight, _ = self.__set_model(
+            kwargs_scaler,
+            kwargs_model,
+            closed,
+            horizon,
+        )
+        if scaler is not None:
+            x_train = scaler.fit_transform(x_train)
+        model.fit(
+            x_train,
+            y_train,
+            sample_weight=train_weight,
+            horizon=np_horizon,
+        )
+        return model, scaler
+
+    def cv_fit(
+        self,
+        data,
+        labels,
+        closed,
+        horizon,
+        kwargs_model=None,
+        kwargs_scaler=None,
+    ):
+        if kwargs_model is None and self._best_kwargs_model is None:
+            raise ValueError(
+                "At least one of the values, kwargs_model or"
+                " _best_kwargs_model, have to be setted"
+            )
+        if kwargs_model is None and self._best_kwargs_model is not None:
+            kwargs_model = self._best_kwargs_model.copy()
+        if kwargs_scaler is None and self._best_kwargs_scaler is None:
+            kwargs_scaler = {}
+        if kwargs_scaler is None and self._best_kwargs_scaler is not None:
+            kwargs_scaler = self._best_kwargs_scaler
+
+        metric_names = list(self._metrics.keys())
+        np_horizon = raw_horizon(closed.index, horizon)
+        base_dir = os.path.join(self._run_dir, "model_fitting")
+        pekfold = PEKFold(n_splits=self._cv_splits_fit)
+        cv_metric_values = np.zeros(len(self._metrics))
+        cv_hist_values = np.zeros((self._cv_splits_fit, len(metric_names)))
+        cv_bar = tqdm(
+            total=self._cv_splits_fit,
+            desc="Processing CV to Assess the Model",
+        )
+
+        cv_step = 0
+        predictions = []
+        spliter = pekfold.split(horizon)
+        for train_idx, test_idx in spliter:
+            outcomes, predicted, index = self.__partial_fit_eval(
+                data,
+                labels,
+                train_idx,
+                test_idx,
+                horizon,
+                np_horizon,
+                closed,
+                kwargs_model,
+                kwargs_scaler,
+            )
+            partial_metric_value = np.array(list(outcomes.values()))
+            cv_hist_values[cv_step] = partial_metric_value
+            cv_metric_values += partial_metric_value
+            cv_step += 1
+            cv_bar.update()
+            cv_bar.set_postfix(**outcomes)
+            predictions.append(pd.Series(predicted, index=index))
+        cv_metric_values /= self._cv_splits_fit
+        cv_metric_values = dict(
+            zip(
+                ["avg_" + name for name in metric_names],
+                cv_metric_values,
+            )
+        )
+        save_hp_performance(
+            base_dir,
+            kwargs_model,
+            kwargs_scaler,
+            cv_metric_values,
+            cv_hist_values.T,
+            metric_names,
+            0,
+        )
+        self._file_log.info(
+            "The fit based on CV were done.\n\t {}".format(cv_metric_values)
+        )
+        model, scaler = self.__fit(
+            data,
+            labels,
+            horizon,
+            np_horizon,
+            closed,
+            kwargs_model,
+            kwargs_scaler,
+        )
+        return model, scaler, predictions, cv_metric_values
 
     def hp_meta_optimize(
         self, data, labels, horizon, closed, hp_model, hp_scaler=None
     ):
-        if self._use_weight:
-            if (self._time_weights_fn is None) and (
-                self._samples_weights_fn is None
-            ):
-                raise ValueError(
-                    "With `use_weight` is True, at least one of the functions,"
-                    " `time_weight_fn` or `sample_weights_fn`, have to be defined"
-                )
         if hp_scaler is None:
             hp_scaler = {}
         grid_model = ParameterGrid(hp_model)
@@ -199,7 +312,7 @@ class BagingModelOptimizer:
             n_steps *= len(v)
         for v in hp_scaler.values():
             n_steps *= len(v)
-        hp_bar = tqdm(total=n_steps, desc="HP Grid Search with CV")
+        hp_bar = tqdm(total=n_steps, desc="Hyper-Parameter Grid Search")
 
         hp_step = 0
         metric_names = list(self._metrics.keys())
@@ -210,7 +323,7 @@ class BagingModelOptimizer:
                 cv_step = 0
                 cv_bar = tqdm(
                     total=self._cv_splits_hp,
-                    desc="KFold CV",
+                    desc="Processing CV for a Hyper-Parameter",
                     leave=False,
                 )
                 cv_hist_values = np.zeros(
@@ -219,7 +332,7 @@ class BagingModelOptimizer:
                 cv_metric_values = np.zeros(len(self._metrics))
                 spliter = pekfold.split(horizon)
                 for train_idx, test_idx in spliter:
-                    outcomes, _, _ = self.__partial_fit_eval(
+                    outcomes, _, _= self.__partial_fit_eval(
                         data,
                         labels,
                         train_idx,
@@ -259,6 +372,16 @@ class BagingModelOptimizer:
                     hp_step,
                 )
                 hp_step += 1
+        self._file_log.info(
+            "The grid search based on CV were done with {} = {}."
+            "\n\t[best model parameters] {}"
+            "\n\t[best scaler parameters] {}".format(
+                metric_names[0],
+                best_metric_value[0],
+                self._best_kwargs_model,
+                self._best_kwargs_scaler,
+            )
+        )
 
     def get_weight(self, closed, horizon, min_chunk_size=100):
         data_size = horizon.shape[0]
@@ -314,15 +437,14 @@ class EnvironmentOptimizer(BagingModelOptimizer):
             # "AUC": (auc, False),
         },
     ):
-        self._log_dir = log_dir
         self._labels_fn = labels_fn
         self._primary_model_fn = primary_model_fn
         self._refined_data = refined_data
         self._cv_splits_hp = cv_splits_hp
-        self._file_log = create_log_file("environment", log_dir=self._log_dir)
         self._run_dir = os.path.join(
-            self._log_dir, author, "run-" + dt.now().strftime("%Y%m%d-%H%M%S")
+            log_dir, author, "run-" + dt.now().strftime("%Y%m%d-%H%M%S")
         )
+        self._file_log = create_log_file("environment", log_dir=self._run_dir)
         super(EnvironmentOptimizer, self).__init__(
             model_fn,
             self._run_dir,
@@ -334,7 +456,6 @@ class EnvironmentOptimizer(BagingModelOptimizer):
             min_time_weight,
             time_weights_fn,
             samples_weights_fn,
-            log_dir,
             metrics,
         )
 
@@ -425,6 +546,7 @@ class EnvironmentOptimizer(BagingModelOptimizer):
         self.hp_meta_optimize(
             data, labels, horizon, closed, hp_model, hp_scaler
         )
+        model, scaler, predictions, metrics = self.cv_fit(data, labels, closed, horizon)
 
     def hp_env_optimize(
         self, hp_model, hp_features, hp_primary, hp_labels, hp_scaler=None
